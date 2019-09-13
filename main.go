2bc4b62a6c5be41ffd768261e49ca6b3e16cdb68
Command <maoxs2@163.com>
2019-09-13 13:31:50 +0800

    [bugFix]put parseConf and logger to the main func

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"github.com/google/logger"
 )
 
-var conf = parseConfig()
-var logFile, _ = os.Create(conf.Log.LogFile)
-var log = logger.Init("pool", conf.Log.Verbose, conf.Log.SystemLog, logFile)
+var log *logger.Logger
 
 func main() {
+	var conf = parseConfig()
+	var logFile, _ = os.Create(conf.Log.LogFile)
+	log = logger.Init("pool", conf.Log.Verbose, conf.Log.SystemLog, logFile)
+
 	db := initDB(conf)
 
 	p := &payer{}
